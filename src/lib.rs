bebb0b45efd3c63c08410a12f92e319ee03b2957
Georges Palauqui <g.palauqui@gptechinno.com>
2025-09-01 15:11:22 +0200

    update defmt to latest v1.0

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ use embedded_hal_async::i2c::I2c as AsyncI2c;
 #[allow(dead_code)]
 #[allow(non_camel_case_types)]
 #[derive(Copy, Clone)]
-#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum Register {
     // Configuration Register
     CONFIG = 0x00,
@@ -62,7 +62,7 @@ impl From<Register> for u8 {
 
 #[allow(dead_code)]
 #[derive(Copy, Clone)]
-#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 /// Averaging Mode
 /// Determines the number of samples that are collected and averaged.
 pub enum Averaging {
@@ -93,7 +93,7 @@ impl Averaging {
 
 #[allow(dead_code)]
 #[derive(Copy, Clone)]
-#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 /// Bus Voltage Conversion Time
 /// Sets the conversion time for the bus voltage measurement
 pub enum BVConvTime {
@@ -124,7 +124,7 @@ impl BVConvTime {
 
 #[allow(dead_code)]
 #[derive(Copy, Clone)]
-#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 /// Shunt Current Conversion Time
 /// Sets the conversion time for the shunt current measurement
 pub enum SCConvTime {
@@ -155,7 +155,7 @@ impl SCConvTime {
 
 #[allow(dead_code)]
 #[derive(Copy, Clone)]
-#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 /// Operating Mode
 /// Selects continuous, triggered, or power-down mode of operation.
 pub enum OperMode {
